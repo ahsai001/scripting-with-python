f3effde39e3cc20b13ecb84a0462f6fe9bb07f56
ahsai001 <ahsai001@gmail.com>
2025-12-12 14:01:17 +0700

    add script git pull all branch with fish

--- git-pull-all.fish
+++ git-pull-all.fish
@@ -1,5 +1,14 @@
+#!/usr/bin/env fish
+
+# Pastikan berada di folder repository
+echo "Repository: (pwd)"
+echo "=============== START PULL ALL BRANCH ==============="
+
 for b in (git branch --format="%(refname:short)")
-    echo "=== Pulling $b ==="
+    echo "=== Switching to $b ==="
     git checkout $b
+    echo "=== Pulling $b ==="
     git pull
 end
+
+echo "=============== DONE ==============="
